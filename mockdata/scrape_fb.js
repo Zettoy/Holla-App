55d233202f39d816cfc3f9826a87106e96d1a23f
Ben Liew <ben.liew.99@gmail.com>
2018-09-13 11:04:22 +1000

    Documentation for scraping js.

--- mockdata/scrape_fb.js
+++ mockdata/scrape_fb.js
@@ -1,12 +1,20 @@
+/*
+*
+*  Run this script on a Facebook page.
+*  E.g. https://www.facebook.com/UNSWRoundhouse/
+*  Make sure the page has finished loading first.
+*
+* */
 (function scrape() {
     function getRandomInt(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
+    var NUM_POSTS_TO_SCRAPE = 3;
 
     var posts = document.querySelectorAll(".userContentWrapper");
     var to_exfil = [];
     posts.forEach(function (post, idx) {
-        if (idx < 3) {
+        if (idx < NUM_POSTS_TO_SCRAPE) {
             // if(idx === 1){
             // console.log(post)
             var see_more = post.querySelector(".see_more_link_inner");
