592c866b2eef0be9662f4c40370cacbd48d19b23
zetto <zhiyuan.yao@student.unsw.edu.au>
2018-09-18 20:21:43 +1000

    close button & locate button work

--- app/src/main/java/com/holla/group1/holla/MakePostActivity.java
+++ app/src/main/java/com/holla/group1/holla/MakePostActivity.java
@@ -1,31 +1,92 @@
 package com.holla.group1.holla;
 
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import org.xml.sax.helpers.LocatorImpl;
 
 public class MakePostActivity extends AppCompatActivity {
+
+    private FusedLocationProviderClient mFusedLocationClient;
+    private Location location;
+    private TextView locationText;
+
+    private EditText post;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_post);
-        /*
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+
+        findViewById(R.id.make_post_close_btn)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        finish();
+                    }
+                });
+
+        findViewById(R.id.send_post_btn)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //TODO: send post
+                    }
+                });
+
+        findViewById(R.id.locate_btn).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                getLocation();
             }
         });
-        */
+
+        locationText = findViewById(R.id.location_text);
+
+        post = findViewById(R.id.post_edit_text);
+
     }
 
+    public void getLocation() {
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        Task t = mFusedLocationClient.getLastLocation();
+        t.addOnCompleteListener(new OnCompleteListener() {
+            @Override
+            public void onComplete(@NonNull Task task) {
+                if (task.isSuccessful()) {
+                    location = (Location) task.getResult();
+                    setLocationText();
+                }
+            }
+        });
+    }
+
+    private void setLocationText() {
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        locationText.setText(latLng.toString());
+    }
 }
