4239e2afc196bea317298e604c866f157ea4b26a
Ben Liew <ben.liew.99@gmail.com>
2018-09-22 15:22:40 +1000

    refactor

--- app/src/main/java/com/holla/group1/holla/MapsActivityUtilities.java
+++ app/src/main/java/com/holla/group1/holla/MapsActivityUtilities.java
@@ -1,7 +1,5 @@
 package com.holla.group1.holla;
 
-import android.app.Activity;
-import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
@@ -12,6 +10,7 @@ public class MapsActivityUtilities {
         return overlayFragment;
 
     }
+
     public static void showOverlay(FragmentActivity activity) {
         FragmentTransaction ft = activity.getSupportFragmentManager().beginTransaction();
         ft.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out);
@@ -25,6 +24,7 @@ public class MapsActivityUtilities {
         ft.hide(getOverlayFragment(activity));
         ft.commit();
     }
+
     public static void setOverlayPost(Post post, FragmentActivity fragmentActivity) {
         PostMapOverlay overlayFragment = (PostMapOverlay) getOverlayFragment(fragmentActivity);
         overlayFragment.showPost(post);
