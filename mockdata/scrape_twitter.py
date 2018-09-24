ca76089207b3d082866734bc5b67ebf05e21fdb1
Ben Liew <ben.liew.99@gmail.com>
2018-09-24 21:17:41 +1000

    some sample tweet ids

--- mockdata/scrape_twitter.py
+++ mockdata/scrape_twitter.py
@@ -1,3 +1,6 @@
+import json
+from pprint import pprint
+import os
 from twitter_api_creds import get_api
 
 
@@ -5,23 +8,43 @@ def scrape(tweet_ids):
     api = get_api()
     statuses = api.GetStatuses(tweet_ids)
     return statuses
+
+
 def transform(status):
     return {
-        "id" : status.id_str,
+        "id": status.id_str,
         "username": status.user.screen_name,
         "name": status.user.name,
-        "likes" : status.favorite_count,
-        "content" : status.text,
-        "created_at_epoch" : status.created_at_in_seconds,
-        "location" : {
-            "coordinates":[],
-            "type" : "Point"
+        "likes": status.favorite_count,
+        "content": status.full_text,
+        "created_at_epoch": status.created_at_in_seconds,
+        "location": {
+            "coordinates": [],
+            "type": "Point"
         },
-        "location_name" : None,
+        "location_name": None,
     }
+
+
+
 if __name__ == '__main__':
-    statuses = scrape([1020082359503212545,
-            1019472406388150273
-            ])
-    transformed = list(map(transform, statuses))
-    print("")
+    DO_OVERRIDE = True
+    from tweets import tweets_to_scrape
+    for domain, to_scrape_list in tweets_to_scrape.items():
+        statuses = scrape(
+            list(map(lambda x:x.tweet_id, to_scrape_list))
+        )
+        transformed = list(map(transform, statuses))
+        for x,status in zip(to_scrape_list, transformed):
+            status["location"] = x.location
+            status["location_name"] = x.location_name
+
+        as_str = json.dumps(transformed, sort_keys=True, indent=4)
+        filepath = os.path.join("new_samples", "{}.json".format(domain))
+        if not DO_OVERRIDE and os.path.exists(filepath):
+            raise Exception("File: {} exists.".format(filepath))
+        else:
+            with open(filepath, "w") as f:
+                f.write(as_str)
+
+
