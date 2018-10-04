cc1a162c05228c07e1a6b6fb829fc1f4fdf9e567
Ben Liew <ben.liew.99@gmail.com>
2018-10-04 10:49:02 +1000

    Started working on multi-search

--- app/src/main/java/com/holla/group1/holla/MultiSearchActivity.java
+++ app/src/main/java/com/holla/group1/holla/MultiSearchActivity.java
@@ -1,22 +1,19 @@
 package com.holla.group1.holla;
 
+import android.os.Bundle;
 import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.SearchView;
 import android.widget.TextView;
 
 public class MultiSearchActivity extends AppCompatActivity {
@@ -64,7 +61,19 @@ public class MultiSearchActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_multi_search, menu);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setMaxWidth(Integer.MAX_VALUE);
+        searchView.setIconifiedByDefault(false);
+
+        searchView.setQueryHint(getString(R.string.search_hint));
+        searchView.requestFocus();
         return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+
+        return super.onPrepareOptionsMenu(menu);
     }
 
     @Override
@@ -75,9 +84,9 @@ public class MultiSearchActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
 
         return super.onOptionsItemSelected(item);
     }
