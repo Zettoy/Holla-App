539fa0b42dfa4b95c7700ae01cc00332104e7c2d
zetto <zhiyuan.yao@student.unsw.edu.au>
2018-10-09 02:35:30 +1100

    Minor changes

--- app/src/main/java/com/holla/group1/holla/FeedFragment.java
+++ app/src/main/java/com/holla/group1/holla/FeedFragment.java
@@ -2,46 +2,31 @@ package com.holla.group1.holla;
 
 import android.content.Context;
 import android.view.View;
+import android.widget.ListView;
 
 import com.holla.group1.holla.post.Post;
 import com.holla.group1.holla.post.PostAdapter;
 import com.holla.group1.holla.post.PostListFragment;
 import com.holla.group1.holla.post.PostLoadTask;
-import org.joda.time.DateTime;
 
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class FeedFragment extends PostListFragment {
 
     @Override
-    protected void initTask() {
-        task = new FeedPostLoadTask(getContext(), posts);
-        task.execute();
+    protected PostLoadTask setTask() {
+        return new FeedPostLoadTask(getContext(), getView(), listView, posts);
     }
 
-    private class FeedPostLoadTask extends PostLoadTask {
+    private static class FeedPostLoadTask extends PostLoadTask {
 
-        public FeedPostLoadTask(Context context, List<Post> posts) {
-            super(context, posts);
+        private FeedPostLoadTask(Context context, View view, ListView listView, List<Post> posts) {
+            super(context, view, listView, posts);
         }
 
         @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-
-            Collections.sort(posts, new Comparator<Post>() {
-                @Override
-                public int compare(Post p1, Post p2) {
-                    DateTime time1 = p1.getCreation_time();
-                    DateTime time2 = p2.getCreation_time();
-
-                    return time2.compareTo(time1);
-                }
-            });
-
-            PostAdapter postAdapter = new PostAdapter(getContext(), R.layout.post_list_item, posts) {
+        protected PostAdapter setPostAdapter() {
+            return new PostAdapter(context.get(), R.layout.post_list_item, posts) {
                 @Override
                 protected String[] setItems() {
                     return new String[]{"Share"};
@@ -55,12 +40,6 @@ public class FeedFragment extends PostListFragment {
                     }
                 }
             };
-            listView.setAdapter(postAdapter);
-
-            if (posts.isEmpty()) {
-                listView.setVisibility(View.INVISIBLE);
-                getView().findViewById(R.id.post_list_empty).setVisibility(View.VISIBLE);
-            }
         }
     }
 }
