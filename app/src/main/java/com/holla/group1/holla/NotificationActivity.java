49ce76c82d96629e8f9408cfa4524fa835f44293
zetto <zhiyuan.yao@student.unsw.edu.au>
2018-10-13 20:10:45 +1100

    Add NotificationActivity

---
+++ app/src/main/java/com/holla/group1/holla/NotificationActivity.java
@@ -0,0 +1,14 @@
+package com.holla.group1.holla;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class NotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+    }
+}
