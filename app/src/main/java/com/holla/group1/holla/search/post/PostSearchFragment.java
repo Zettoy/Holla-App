33fc9d09941f15e9100ca21f849fddab148aba44
Ben Liew <ben.liew.99@gmail.com>
2018-10-15 20:48:55 +1100

    Started work on search for posts by content

---
+++ app/src/main/java/com/holla/group1/holla/search/post/PostSearchFragment.java
@@ -0,0 +1,21 @@
+package com.holla.group1.holla.search.post;
+
+import com.holla.group1.holla.post.PostListFragment;
+
+public class PostSearchFragment extends PostListFragment {
+    @Override
+    protected void readPostsFromBackend() {
+
+    }
+
+    @Override
+    protected String[] onCreateMenuItems() {
+        return new String[0];
+    }
+
+    @Override
+    protected void onMenuOptionItemSelected(int which) {
+
+    }
+
+}
