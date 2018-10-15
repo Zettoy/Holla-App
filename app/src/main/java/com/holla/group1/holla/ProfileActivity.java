00309f00f0e36d65fe3107b6040cafe5a8b91684
stewartconnor <sunbeat3@gmail.com>
2018-10-15 22:36:24 +1100

    Added following connecting to the backend

--- app/src/main/java/com/holla/group1/holla/ProfileActivity.java
+++ app/src/main/java/com/holla/group1/holla/ProfileActivity.java
@@ -6,25 +6,39 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
-public class ProfileActivity extends AppCompatActivity {
+import com.holla.group1.holla.api.RestAPIClient;
+
+public class ProfileActivity extends AppCompatActivity  {
     private HistoryFragment historyFragment;
     private ListView historyPostListView;
     private String userName;
     private String userID;
+    private Button followBtn;
+    private boolean alreadyFollowing = false;
+    private RestAPIClient apiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
 
         Intent intent = getIntent();
         userName = intent.getStringExtra("userName");
         userID = intent.getStringExtra("userID");
 
-        //Todo: Call backend to see if we need to greyout follow/make it unfollow
+        TextView usernameTxt = (TextView) findViewById(R.id.usernameTxt);
+        if (userName != null) usernameTxt.setText(userName);
 
-        setContentView(R.layout.activity_profile);
+        followBtn = (Button) findViewById(R.id.followBtn);
+        followBtn.setOnClickListener(new FollowBtnListener());
+        apiClient = new RestAPIClient(this, null, null);
+
+        //Todo: Call backend to see if we need to greyout follow/make it unfollow
 
         Toolbar toolbar = findViewById(R.id.activity_history_toolbar);
         setSupportActionBar(toolbar);
@@ -50,4 +64,18 @@ public class ProfileActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private class FollowBtnListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            if (alreadyFollowing) {
+                followBtn.setText("Follow");
+                apiClient.unfollowUser(userID);
+                alreadyFollowing = false;
+            } else {
+                followBtn.setText("Unfollow");
+                apiClient.followUser(userID);
+                alreadyFollowing = true;
+            }
+        }
+    }
 }
