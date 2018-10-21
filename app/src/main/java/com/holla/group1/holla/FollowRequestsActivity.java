88485b1036e49c88ca27a0b843c8386aadd26ea2
stewartconnor <sunbeat3@gmail.com>
2018-10-21 21:06:14 +1100

    Nicer looking follow requests

--- app/src/main/java/com/holla/group1/holla/FollowRequestsActivity.java
+++ app/src/main/java/com/holla/group1/holla/FollowRequestsActivity.java
@@ -2,8 +2,12 @@ package com.holla.group1.holla;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Pair;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.holla.group1.holla.R;
 import com.holla.group1.holla.api.RestAPIClient;
@@ -12,15 +16,25 @@ import com.holla.group1.holla.user.User;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FollowRequestsActivity extends ListActivity implements RestAPIClient.OnGetFollowRequestsLoadedListener {
+public class FollowRequestsActivity extends AppCompatActivity implements RestAPIClient.OnGetFollowRequestsLoadedListener {
     //private TextView text;
     private List<String> listValues;
     private RestAPIClient apiClient;
+    private ListView requestList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_follow_requests);
+
+        Toolbar toolbar = findViewById(R.id.activity_history_toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowTitleEnabled(false);
+        }
 
         apiClient = new RestAPIClient(this, null, null);
         apiClient.setOnGetFollowRequestsLoadedListener(this);
@@ -30,7 +44,8 @@ public class FollowRequestsActivity extends ListActivity implements RestAPIClien
     @Override
     public void OnGetFollowRequestsLoaded(List<Pair<String,String>> users) {
         FollowRequestAdapter adapter = new FollowRequestAdapter(this, (ArrayList<Pair<String, String>>) users);
-        setListAdapter(adapter);
+        requestList = (ListView) findViewById(R.id.request_list);
+        requestList.setAdapter(adapter);
         adapter.setApi(apiClient);
     }
 
